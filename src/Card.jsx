466aebc5f6c241d85e4b02895fef3318d6f181bd
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-23 01:49:46 +0800

    Createa card components

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+  return(
+    <div className="card">
+      <img className="card-image" src="https://via.placeholder.com/150" alt="Profile Picture" />
+      <h2 className="card-title">Rhyu</h2>
+      <p className="card-text">I am studying React for the Hackaton</p>
+    </div>
+  );
+}
+
+export default Card
